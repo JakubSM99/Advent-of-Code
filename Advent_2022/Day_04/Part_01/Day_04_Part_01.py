67ddedeba76af5df43523ffbe6881a4d3c4e80d2
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-12-08 20:13:44 +0100

    idk

--- Advent_2022/Day_04/Part_01/Day_04_Part_01.py
+++ Advent_2022/Day_04/Part_01/Day_04_Part_01.py
@@ -1,4 +1,4 @@
-with open('C:/Users/Jakub/Documents/GitHub/Advent-of-Code-2021/Advent_2022/Day_04/Part_01/Test_input.txt','r') as f:
+with open('C:/Users/Jakub/Documents/GitHub/Advent-of-Code-2021/Advent_2022/Day_04/Part_01/Official_input.txt','r') as f:
     line = f.readlines()
 
     elements = []
@@ -21,21 +21,14 @@ with open('C:/Users/Jakub/Documents/GitHub/Advent-of-Code-2021/Advent_2022/Day_0
         elf_one = two_elfs[0]
         elf_two = two_elfs[1]
 
-
         elf1_min = int(elf_one[0])
         elf1_max = int(elf_one[1])
         elf2_min = int(elf_two[0])
         elf2_max = int(elf_two[1])
         
-        print(elf_one, elf_two)
-        if elf1_min >= elf2_min:
-            if elf1_max <= elf2_max:
-                count += 1
-                print("Pierwszy w drugim")
-        elif elf2_min >= elf1_min:
-            if elf2_max <= elf1_max:
-                count += 1
-                print("Drugi w pierwszym")
+        if (elf2_min <= elf1_max) and (elf2_min >= elf1_min):
+            count += 1
+        elif (elf1_min <= elf2_max) and (elf1_min >= elf2_min):
+            count += 1
 
-    
     print(count)
---
+++ Advent_2022/Day_05/Part_01/Day_05_Part_01.py
@@ -0,0 +1,9 @@
+with open('C:/Users/Jakub/Documents/GitHub/Advent-of-Code-2021/Advent_2022/Day_05/Part_01/Test_input.txt','r') as f:
+    lines = f.readlines()
+
+    for line in lines:
+        b = line.strip()
+        a = b.split(" ")
+
+        print(a)
+            
